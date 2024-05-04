ed085dd5a0dbab75d42f3fb25c08e47442567ce9
unixlinuxgeek <unixlinuxgeek@gmail.com>
2024-05-04 14:35:01 +0500

    Fix

--- lenconv_test.go
+++ lenconv_test.go
@@ -8,14 +8,13 @@ import (
 // делим значение длины на 3.281
 func TestFtToMet(t *testing.T) {
 	r := floatgen.GenRan(1, 9)
-	o := r / 0.3048
+	o := r / 3.281
 	b := float64(FtToMet(Ft(r)))
 
-	t.Log(o, b)
 	if o == b {
-		t.Logf("Test Passed!!! %v equal %v\n", o, b)
+		t.Logf("%v equal %v\n", o, b)
 	} else {
-		t.Fatalf("Test Failed %v not equal %v\n", o, b)
+		t.Fatalf("%v not equal %v\n", o, b)
 	}
 }
 
